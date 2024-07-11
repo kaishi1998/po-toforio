6978dc216220ff1f8b708de2ebea44976ff7e679
kaishi1998 <kaishi.sakigake0221@docomo.ne.jp>
2024-07-11 23:34:31 +0900

    確認をお願いします。７月11日

---
+++ nagoyameshi/src/main/java/com/example/nagoyameshi/repository/HouseRepository.java
@@ -0,0 +1,18 @@
+package com.example.nagoyameshi.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.nagoyameshi.entity.House;
+
+
+public interface HouseRepository extends JpaRepository<House, Integer>{
+	public Page<House> findByNameLike(String keyword,Pageable pageable);
+	
+	public Page<House> findByNameLikeOrAddressLike(String nameKeyword, String addressKeyword, Pageable pageable);
+	public Page<House> findByAddressLike(String area, Pageable pageable);
+	public Page<House> findByPriceLessThanEqual(Integer price, Pageable pageable);
+		
+	}
+
